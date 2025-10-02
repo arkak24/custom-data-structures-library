5df3a3817689bf8fd25f4e75740da52057e4639f
arkak24 <arkak.brj@gmail.com>
2025-10-02 12:16:38 +0530

    vector basic implementation

---
+++ include/vector.hpp
@@ -0,0 +1,75 @@
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
+#include <cstddef>
+
+namespace custom {
+
+      template <typename T>
+            class Vector{
+                  private:
+                        T* data;
+                        std::size_t no_of_current_elements; // sz
+                        std::size_t curr_capacity; // cap
+
+                  public:
+                        Vector(){
+                              no_of_current_elements = 0;
+                              curr_capacity = 5;
+                              data = new T[curr_capacity];
+                        };
+
+                        ~Vector(){
+                              delete[] data;
+                        };
+
+                        void push_back(const T& value){
+                              if(no_of_current_elements == curr_capacity){
+                                    std::size_t new_capacity = curr_capacity*2;
+                                    T* new_data = new T[new_capacity];
+                                    for(std::size_t i = 0; i < no_of_current_elements; i++){
+                                          new_data[i] = data[i];
+                                    }
+                                    delete[] data;
+                                    data = new_data;
+                                    curr_capacity = new_capacity;
+                              }
+                              data[no_of_current_elements] = value;
+                              no_of_current_elements += 1;
+                        };
+
+                        void pop_back(){
+                              if(no_of_current_elements > 0){
+                                    no_of_current_elements -= 1;
+                              }
+                        };
+
+                        T& operator[](std::size_t index){
+                              return data[index];
+                        };
+
+                        const T& operator[](std::size_t index) const{
+                              return data[index];
+                        };
+
+                        std::size_t size() const{
+                              return no_of_current_elements;
+                        };
+
+                        std::size_t capacity() const{
+                              return curr_capacity;
+                        };
+
+                        bool is_empty() const{
+                              if(no_of_current_elements == 0){
+                                    return true;
+                              }
+                              else{
+                                    return false;
+                              }
+                        };
+            };
+
+}
+
+#endif
